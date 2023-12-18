7b389b7e862e1c8a8d7f5b990d831356e2fb9b11
mj11jm <themj11jm@gmail.com>
2023-12-18 09:14:17 +0000

    Basic menu navigation test

--- commands/preferences.js
+++ commands/preferences.js
@@ -7,6 +7,8 @@ const {
     Button
 } = require("../staticdata/components")
 
+const btnReturn = new Button('preferenceReturn').setLabel('Main Menu').setStyle(2)
+
 const preferences = {
     all: [
         {
@@ -23,6 +25,102 @@ const preferences = {
             description: 'Display All Profile Preferences',
             label: 'Profile Preferences',
             value: 'profile'
+        }
+    ],
+    interact: [
+        {
+            description: '',
+            label: 'Can Has',
+            value: 'interact_canhas'
+        },
+        {
+            description: '',
+            label: 'Can Diff',
+            value: 'interact_candiff'
+        },
+        {
+            description: '',
+            label: 'Can Sell',
+            value: 'interact_cansell'
+        }
+    ],
+    notify: [
+        {
+            description: '',
+            label: 'First Bid On Your Auctions',
+            value: 'notify_aucbidme'
+        },
+        {
+            description: '',
+            label: 'Outbid On Auction Notification',
+            value: 'notify_aucoutbid'
+        },
+        {
+            description: '',
+            label: 'New Bid On Auction Notification',
+            value: 'notify_aucnewbid'
+        },
+        {
+            description: '',
+            label: 'Auction End Notification',
+            value: 'notify_aucend'
+        },
+        {
+            description: '',
+            label: 'New Bot Announcements',
+            value: 'notify_announce'
+        },
+        {
+            description: '',
+            label: 'Daily Ready Notifications',
+            value: 'notify_daily'
+        },
+        {
+            description: '',
+            label: 'Vote Ready Notifications',
+            value: 'notify_vote'
+        },
+        {
+            description: '',
+            label: 'Collection Completion Status Notifications',
+            value: 'notify_completed'
+        },
+        {
+            description: '',
+            label: 'Effect Expires Or Comes Off Cooldown',
+            value: 'notify_effectend'
+        }
+    ],
+    profile: [
+        {
+            description: '',
+            label: 'Set a custom bio',
+            value: 'profile_bio'
+        },
+        {
+            description: '',
+            label: 'Choose a custom title',
+            value: 'profile_title'
+        },
+        {
+            description: '',
+            label: 'Set a custom color',
+            value: 'profile_color'
+        },
+        {
+            description: '',
+            label: 'Display your favorite card',
+            value: 'profile_card'
+        },
+        {
+            description: '',
+            label: 'Set your favorite completed collection',
+            value: 'profile_favcomplete'
+        },
+        {
+            description: '',
+            label: 'Set your favorite clouted collection',
+            value: 'profile_favclout'
         }
     ]
 }
@@ -43,6 +141,9 @@ cmd(['preferences', 'show', 'notify'], async (ctx, user, args) => await defaultF
 
 rct('preferenceMenu', async (ctx, user, args) => await displayMenu(ctx, user, args))
 rct('preferenceReturn', async (ctx, user, args) => await prefsMain(ctx, user, args, true))
+rct('preferenceItem', async (ctx, user, args) => await displayPref(ctx, user, args))
+rct('preferenceDisable', async (ctx, user, args) => await prefDisable(ctx, user, args))
+rct('preferenceEnable', async (ctx, user, args) => await prefEnable(ctx, user, args))
 
 
 const prefsMain = async (ctx, user, args, back = false) => {
@@ -54,11 +155,43 @@ const prefsMain = async (ctx, user, args, back = false) => {
 }
 
 const displayMenu = async (ctx, user, args) => {
-    const btn = new Button('preferenceReturn').setLabel('Main Menu').setStyle(2)
-    await ctx.send(ctx, user, {buttons: [btn], content: `This will soon show the ${ctx.id.pop()} menu....`, parent: true})
+    const menu = ctx.id.pop()
+    const selectMenu = {type: 3, customID: 'preferenceItem', options: preferences[menu]}
+    await ctx.send(ctx, user, {select: [selectMenu], buttons: [btnReturn], content: `Please select the option you want to change from the ${menu}`, parent: true})
     // await ctx.interaction.editParent({content: `This will soon show the ${ctx.id.pop()} menu....`, components: [btn]})
 }
 
+const displayPref = async (ctx, user, args) => {
+    const options = ctx.id.pop().split('_')
+    if (options[0] === 'profile') {
+        return await ctx.reply(user, `profile options currently don't function.`, 'red', {buttons: [btnReturn], parent: true})
+    }
+    const isEnabled = user.prefs[options[0]][options[1]]
+    const btnEnable = new Button(`preferenceEnable_${options[0]}_${options[1]}`).setStyle(3).setLabel('Enable').setOff(isEnabled)
+    const btnDisable = new Button(`preferenceDisable_${options[0]}_${options[1]}`).setStyle(4).setLabel('Disable').setOff(!isEnabled)
+    await ctx.send(ctx, user, {buttons: [btnReturn, btnDisable, btnEnable], content: `This is where an embed will show info for ${options[1]}`, parent: true})
+}
+
+const prefEnable = async (ctx, user, args) => {
+    const section = ctx.id[0]
+    const option = ctx.id[1]
+    console.log(ctx.id)
+    user.prefs[section][option] = true
+    await user.save()
+    const btnEnable = new Button(`preferenceEnable_${section}_${option}`).setStyle(3).setLabel('Enable').setOff(true)
+    const btnDisable = new Button(`preferenceDisable_${section}_${option}`).setStyle(4).setLabel('Disable').setOff(false)
+    await ctx.send(ctx, user, {buttons: [btnReturn, btnDisable, btnEnable], content: `This is where an embed will show info for ${ctx.id[1]}.\nCurrently ${user.prefs[section][option]}`, parent: true})
+}
+
+const prefDisable = async (ctx, user, args) => {
+    const section = ctx.id[0]
+    const option = ctx.id[1]
+    user.prefs[section][option] = false
+    await user.save()
+    const btnEnable = new Button(`preferenceEnable_${section}_${option}`).setStyle(3).setLabel('Enable').setOff(false)
+    const btnDisable = new Button(`preferenceDisable_${section}_${option}`).setStyle(4).setLabel('Disable').setOff(true)
+    await ctx.send(ctx, user, {buttons: [btnReturn, btnDisable, btnEnable], content: `This is where an embed will show info for ${ctx.id[1]}.\nCurrently ${user.prefs[section][option]}`, parent: true})
+}
 
 
 
