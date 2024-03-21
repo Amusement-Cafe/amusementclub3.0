11184e1b1a11be519cb67e2b4cc80e45a5b0788e
mj11jm <themj11jm@gmail.com>
2024-03-21 00:13:48 +0000

    Add Draw, Forge. Update withCards for forging

--- modules/card.js
+++ modules/card.js
@@ -26,6 +26,26 @@ const formatCard = (ctx, card) => {
 const withCards = (callback, options) => async (ctx, user, args) => {
     let cards
 
+
+    if (args.forgeArgs1) {
+        cards = await getAllUserCards(user, true)
+        cards = cards.filter(x => x.cardID < ctx.cards.length).map(x => Object.assign({}, ctx.cards[x.cardID], x))
+        let batchCards1 = cards
+        let batchCards2 = cards
+
+        args.forgeArgs1.filters?.map(x => batchCards1 = batchCards1.filter(x))
+        batchCards1.sort(args.forgeArgs1.sort)
+
+        if (args.forgeArgs2) {
+            args.forgeArgs2.filters?.map(x => batchCards2 = batchCards2.filter(x))
+            batchCards2.sort(args.forgeArgs2.sort)
+        } else {
+            batchCards2 = batchCards1
+        }
+
+        return callback(ctx, user, args, [batchCards1, batchCards2])
+    }
+
     if (!options?.global || args.userQuery) {
         cards = await getAllUserCards(user)
 
@@ -47,7 +67,7 @@ const withCards = (callback, options) => async (ctx, user, args) => {
     if (!options?.allowEmpty && cards.length === 0)
         return ctx.reply(user, `no cards found`)
 
-    cards.sort(args.cardQuery? args.cardQuery.sort: firstBy((a, b) => b.level - a.level).thenBy("col").thenBy("name"))
+    cards.sort(args.cardQuery.sort)
 
     return callback(ctx, user, args, cards)
 }
