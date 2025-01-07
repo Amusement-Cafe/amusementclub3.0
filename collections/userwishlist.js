c8298594b72d21488812247ec43779072156d438
mj11jm <themj11jm@gmail.com>
2025-01-07 22:29:38 +0000

    Let's start again

--- collections/userwishlist.js
+++
@@ -1,9 +0,0 @@
-const {model, Schema} = require('mongoose')
-
-const schema = new Schema({
-    userID:         { type: String, index: true },
-    cardID:         { type: Number, index: true },
-
-    added:          { type: Date, default: Date.now }
-})
-module.exports = model('UserWishlist', schema)
