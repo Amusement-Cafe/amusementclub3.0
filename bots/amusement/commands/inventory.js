beee05d90181d6b4bae9fd7297661c18e454c593
mj11jm <themj11jm@gmail.com>
2025-12-28 19:01:01 +0000

    Self contained pagination for tickets in inventory

--- bots/amusement/commands/inventory.js
+++ bots/amusement/commands/inventory.js
@@ -21,6 +21,7 @@ registerBotCommand(['inventory'], async (ctx) => await inventoryStart(ctx))
 registerReaction(['inventoryMain'], async (ctx) => await mainSelect(ctx))
 registerReaction(['inventoryItem'], async (ctx) => await itemSelect(ctx))
 registerReaction(['inv', 'home'], async (ctx) => await inventoryStart(ctx, true))
+registerReaction(['ticket', 'page'], async (ctx) => await ticketPage(ctx))
 
 const inventoryStart = async (ctx, back = false) => {
     const inv = await getUserInventory(ctx)
@@ -63,17 +64,13 @@ const mainSelect = async (ctx) => {
 const itemSelect = async (ctx) => {
     let inv = await getUserInventory(ctx)
     const item = ctx.arguments[0]
-    console.log(inv)
-    console.log(item)
     inv = inv.filter(x => item.split('-')[0] === x.itemID)
-    console.log(inv)
     if (inv.length === 0) {
         return ctx.send(ctx, `something went wrong`, 'red')
     }
     switch (item.split('-')[1]) {
         case 'ticket':
-            await ticketSelect(ctx, inv)
-            break
+            return await ticketSelect(ctx, inv)
         case 'bonus':
         case 'building':
         case 'recipe':
@@ -92,7 +89,6 @@ const itemSelect = async (ctx) => {
 
 const ticketSelect = async (ctx, inv) => {
     const item = ctx.items[ctx.arguments[0].split('-')[0]]
-    console.log(inv)
     let invItems = inv.reduce((acc, current) => {
         const exists = acc.find(item => item.collectionID === current.collectionID)
         if (!exists) {
@@ -101,20 +97,79 @@ const ticketSelect = async (ctx, inv) => {
             return acc
         }
     }, [])
+    let buttons = [homeButton]
+    let pgnButtons = []
     if (invItems.length > 1) {
-        console.log(invItems)
-        console.log('more than one')
-    } else {
-        console.log(invItems)
-        console.log('only one')
+        pgnButtons.push(new Button(`ticket_page-1-${item.itemID}-${item.type}`).setLabel('Next').setStyle(1))
+        if (invItems.length > 2){
+            pgnButtons.push(new Button(`ticket_page-${invItems.length - 1}-${item.itemID}-${item.type}`).setLabel('Last').setStyle(1))
+        }
     }
 
+
     return ctx.send(ctx, {
-        pages: invItems,
-        switchPage: (data) => console.log(data),
+        pages: invItems.map(x => x.collectionID || 'random'),
         embed: {
-            description: 'test'
+            title: ctx.items[invItems[0].itemID].itemID,
+            description: invItems[0].collectionID? invItems[0].collectionID: 'random'
+        },
+        customPgnButtons: pgnButtons.length !== 0? pgnButtons: false,
+        customButtons: buttons,
+        parent: true
+    })
+}
+
+const ticketPage = async (ctx) => {
+    let page = Number(ctx.arguments.shift())
+
+    let itemID = ctx.arguments.shift()
+    let type = ctx.arguments.shift()
+    let inv = await getUserInventory(ctx, type)
+    inv = inv.filter(x => x.itemID === itemID)
+    let invItems = inv.reduce((acc, current) => {
+        const exists = acc.find(item => item.collectionID === current.collectionID)
+        if (!exists) {
+            return acc.concat([current])
+        } else {
+            return acc
         }
+    }, [])
+    if (page < 0) {
+        page = invItems.length - 1
+    }
+    if (page > invItems.length - 1) {
+        page = 0
+    }
+    let filteredItem = invItems[page]
+    let pages = invItems.map(x => x.collectionID || 'random')
+    let index = pages.findIndex(item => filteredItem.collectionID === item)
+    if (index !== -1) {
+        const [removedItem] = pages.splice(index, 1)
+        pages.unshift(removedItem)
+    }
+    let pgnButtons = []
+    if (invItems.length > 1) {
+        pgnButtons.push(new Button(`ticket_page-${page - 1 < 0? invItems.length - 1: page - 1}-${itemID}-${type}`).setLabel('Back').setStyle(1))
+        pgnButtons.push(new Button(`ticket_page-${page + 1}-${itemID}-${type}`).setLabel('Next').setStyle(1))
+        if (page > 1) {
+            pgnButtons.push(new Button(`ticket_page-0-${itemID}-${type}`).setLabel('First').setStyle(1))
+        }
+        if (page !== invItems.length - 2 && page !== invItems.length - 1 && page !== 0) {
+            pgnButtons.push(new Button(`ticket_page-${invItems.length - 1}-${itemID}-${type}`).setLabel('Last').setStyle(1))
+        }
+    }
+    return ctx.send(ctx, {
+        pages: pages,
+        embed: {
+            title: ctx.items[invItems[0].itemID].itemID,
+            description: pages[0],
+            footer: {
+                text: `Page ${page+1}/${invItems.length}`,
+            }
+        },
+        parent: true,
+        customButtons: [homeButton],
+        customPgnButtons: pgnButtons
     })
 }
 
