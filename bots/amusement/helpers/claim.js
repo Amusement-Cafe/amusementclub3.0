7bbc78859dedf946bd4fe7f284fc261068584e35
mj11jm <themj11jm@gmail.com>
2026-01-19 00:26:49 +0000

    Begin process for promo bal

--- bots/amusement/helpers/claim.js
+++ bots/amusement/helpers/claim.js
@@ -1,15 +1,16 @@
-const calculateClaimCost = (ctx, count, claimCount) => {
+const calculateClaimCost = (ctx, count, claimCount, promo = false) => {
     const activeDiscount = ctx.promos.find(x => x.isDiscount === true && x.expires > new Date() && x.starts < new Date())
 
     // const activeDiscount = 100
     let price = 0
-    let cost = activeDiscount? Math.floor(50 * (activeDiscount.discount/ 100)): 50
+    let base = promo? 25: 50
+    let cost = activeDiscount && !promo? Math.floor(base * (activeDiscount.discount/ 100)): base
     let claims = claimCount || 0
     for (let i = 0; i < count; i++) {
         claims++
         price += claims * cost
     }
-    return price
+    return Math.round(price)
 }
 
 module.exports = {
