41b62ac157d5a421dd05dcad4868b2602cca2789
mj11jm <themj11jm@gmail.com>
2026-01-30 01:01:15 +0000

    Fix spelling/grammar

--- bots/amusement/commands/eval.js
+++ bots/amusement/commands/eval.js
@@ -19,7 +19,7 @@ const eval = async (ctx, many = false, global = false) => {
             return ctx.send(ctx, `${ctx.formatName(ctx, ctx.globalCards[0])} is worth ${ctx.boldName(ctx.fmtNum(ctx.globalCards[0].eval))} ${ctx.symbols.tomato}`, 'blue')
         }
         evals = ctx.globalCards.reduce((acc, card) => {return acc + card.eval}, 0)
-        return ctx.send(ctx, `Your query consisting of ${ctx.boldName(ctx.fmtNum(ctx.globalCards.length))} card(s) is worth ${ctx.boldName(ctx.fmtNum(evals))} ${ctx.symbols.tomato}`)
+        return ctx.send(ctx, `Your query consisting of ${ctx.boldName(ctx.fmtNum(ctx.globalCards.length))} cards is worth ${ctx.boldName(ctx.fmtNum(evals))} ${ctx.symbols.tomato}`)
     }
 
     if (!many) {
@@ -27,5 +27,5 @@ const eval = async (ctx, many = false, global = false) => {
     }
     let totalCopies = 0
     evals = ctx.userCards.reduce((acc, card) => {totalCopies += card.amount; return acc + (multi? card.amount * card.eval: card.eval)}, 0)
-    return ctx.send(ctx, `Your query consisting of ${ctx.boldName(ctx.fmtNum(totalCopies))} card is worth ${ctx.boldName(ctx.fmtNum(evals))} ${ctx.symbols.tomato}`)
+    return ctx.send(ctx, `Your query consisting of ${ctx.boldName(ctx.fmtNum(totalCopies))} cards is worth ${ctx.boldName(ctx.fmtNum(evals))} ${ctx.symbols.tomato}`)
 }
