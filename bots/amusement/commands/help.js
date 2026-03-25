d3e9181f68524e3c0074c8846308a281b341c641
mj11jm <themj11jm@gmail.com>
2026-03-25 23:47:58 +0000

    Start on help command

--- bots/amusement/commands/help.js
+++ bots/amusement/commands/help.js
@@ -1,8 +1,59 @@
-const {registerBotCommand} = require('../../../utils/commandRegistrar')
+const {
+    registerBotCommand,
+    registerReaction
+} = require('../../../utils/commandRegistrar')
 
+const {
+    Button,
+    Selection
+} = require("../helpers/componentBuilders")
+
+const {
+    main,
+    subMenus,
+} = require('../static/embeds/help')
+
+const helpSelect = require('../static/menus/help/help.json')
+
+const startPage = new Button('helpStart').setLabel('Main Menu').setStyle(2)
 
 registerBotCommand('help', async (ctx) => await helpStart(ctx))
+registerReaction('help', async (ctx) => await displayHelpPage(ctx))
+registerReaction('helpStart', async (ctx) => await helpStart(ctx, true))
 
-const helpStart = async (ctx) => {
-    return ctx.send(ctx, `The help menu is currently a WIP and subject to change at any time. At this time, you have no help menu.`, 'red')
+
+const helpStart = async (ctx, back = false) => {
+    const senko = 'No help for you\n```⠀⠀⠀⠀⠀⠀⠀⠀⣾⢆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⢧⠀⠀⠀⠀⠀⠀⠀\n' +
+        '⠀⠀⠀⠀⠀⠀⠀⢰⣿⠈⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⡿⢸⠀⠀⠀⠀⠀⠀⠀\n' +
+        '⠀⠀⠀⠀⠀⠀⢠⣿⡿⠀⢸⢀⡀⠀⠀⠀⠀⠀⠀⣰⣿⠃⢸⠀⠀⠀⠀⠀⠀⠀\n' +
+        '⠀⠀⠀⠀⠀⠀⣾⣿⠟⠛⠞⢹⣁⣀⣀⣀⣀⣤⣾⣿⡿⠴⢾⢤⠄⠀⠀⠀⠀⠀\n' +
+        '⠀⠀⠀⠀⠀⣼⣿⣿⣀⣀⣜⣯⠁⠀⠈⠉⠉⠉⠛⠻⠦⣄⠀⣚⠦⠀⠀⠀⠀⠀\n' +
+        '⠀⠀⠀⢀⣼⠿⠿⠏⠈⠛⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠲⠤⣀⣀⡀⠀⠀\n' +
+        '⠀⠀⢀⡜⠁⠀⠀⣠⡄⠀⠀⠀⠀⢀⡀⠀⠀⠀⠀⠀⣦⠀⢀⣀⠀⠲⢍⣀⣀⠀\n' +
+        '⠀⠀⡞⠀⠀⠀⢀⣿⠁⠀⠀⢀⣀⡸⣇⡀⠸⡇⠀⠀⡿⣆⣀⠹⣆⠀⣀⠴⠋⠀\n' +
+        '⠀⢸⣷⣦⣤⣀⣾⠃⠀⠀⠀⣼⣏⣉⣉⡛⣾⠿⠇⢰⡭⢽⣧⠀⣿⡋⠁⠀⠀⠀\n' +
+        '⢀⣾⣿⣿⣿⣿⠃⠀⠀⠀⢀⣾⠋⣤⣶⣐⠋⠀⣴⠃⣶⣶⡘⢷⣿⣷⡄⠀⠀⠀\n' +
+        '⣿⣿⣿⣿⡿⠁⠀⣠⣶⢀⣼⡻⠴⠿⠛⠛⠀⠀⠀⠀⠙⠛⠓⠚⠻⣿⣿⣶⣤⣀\n' +
+        '⣿⣿⣿⣯⣥⣴⣾⢻⣿⠞⢺⢷⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⡏⠁⠀\n' +
+        '⠿⠿⠿⣿⣿⣿⣿⣧⣀⠀⠀⢃⠀⠀⠀⠀⠈⠉⠉⠁⠀⠀⠀⢀⣼⣿⣿⣷⠀⠀\n' +
+        '⠀⠀⣰⣿⣿⣿⣿⣿⣿⣿⣶⣼⣦⠀⠀⠀⠀⠀⠀⠀⢀⣀⣴⣿⣿⢻⣿⡟⠀⠀```'
+    const display = {...main}
+    display.color = ctx.colors.blue
+    display.description += `\n${senko}\n`
+    const select = new Selection('help').setOptions(helpSelect.all)
+    return ctx.send(ctx, {
+        embed: display,
+        selection: [select],
+        customButtons: [startPage],
+        parent: back
+    })
+}
+
+const displayHelpPage = async (ctx) => {
+    const display = subMenus[ctx.arguments[0]]
+    return ctx.send(ctx, {
+        embed: display,
+        customButtons: [startPage],
+        parent: true
+    })
 }
---
+++ bots/amusement/static/embeds/help.js
@@ -0,0 +1,64 @@
+const main = {
+    title: "Help",
+    description: "Help usage",
+}
+
+const items = {
+    title: "Help",
+    description: "Help usage",
+}
+
+const currencies = {
+    title: "Help",
+    description: "Help usage",
+}
+
+const cards = {
+    title: "Help",
+    description: "Help usage",
+}
+
+const collections = {
+    title: "Help",
+    description: "Help usage",
+}
+
+const guilds = {
+    title: "Help",
+    description: "Help usage",
+}
+
+const plots = {
+    title: "Help",
+    description: "Help usage",
+}
+
+const forge = {
+    title: "Help",
+    description: "Help usage",
+}
+
+const auction = {
+    title: "Help",
+    description: "Help usage",
+}
+
+const store = {
+    title: "Help",
+    description: "Help usage",
+}
+
+module.exports = {
+    main,
+    subMenus: {
+        auction,
+        cards,
+        collections,
+        currencies,
+        forge,
+        guilds,
+        items,
+        plots,
+        store
+    }
+}
