2e96833ea2c22141035255176e8b0abbc6b0827f
mj11jm <themj11jm@gmail.com>
2026-04-16 20:41:08 +0000

    April fools

--- utils/webhooks.js
+++ utils/webhooks.js
@@ -12,6 +12,7 @@ const listen = async (ctx) => {
 
     const app = express()
     app.get('/id/*', async (req, res) => {
+        const date = new Date()
         let cardID = req.url.split('/')[2]
         if (cardID.endsWith('.jpg') || cardID.endsWith('.png') || cardID.endsWith('.gif')) {
             cardID = cardID.toString().substring(0, cardID.length - 4)
@@ -24,7 +25,11 @@ const listen = async (ctx) => {
             res.status(404).send('No card with that ID').end()
         }
         let collection = await Collection.findOne({collectionID: card.collectionID})
-        res.redirect(301, `https://t.amu.cards/cards/${card.collectionID}/${card.rarity}_${card.cardName.replaceAll(' ', '_')}${card.animated? '.gif': collection.compressed? '.jpg': '.png'}`)
+        if (date.getDate() === 1 && date.getMonth() === 3 && date.getMinutes() % 5 === 0) {
+            res.redirect(301, 'https://t.amu.cards/cards/astley.jpg')
+        } else {
+            res.redirect(301, `https://t.amu.cards/cards/${card.collectionID}/${card.rarity}_${card.cardName.replaceAll(' ', '_')}${card.animated? '.gif': collection.compressed? '.jpg': '.png'}`)
+        }
         return res.status(200).end()
     })
 
